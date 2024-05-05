c4da449df45a7336320bf41d2ba868c0d87d0ff6
User <User@DESKTOP-TJACIBS.home>
2024-05-05 10:31:21 -0400

    test

--- ProjectSelenium/src/test/java/testcase1/Ordering.java
+++ ProjectSelenium/src/test/java/testcase1/Ordering.java
@@ -9,7 +9,7 @@ import pages1.TitlePage;
  
 	public class Ordering extends Basetest1{     
 		
-		@Test //praveena
+		@Test //praveena   234234234
 		public void ordering() throws InterruptedException, IOException {
 			/*new HomePage(driver).scrolltoElement();
 			Thread.sleep(2000);
